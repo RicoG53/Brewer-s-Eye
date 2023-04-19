0a63e02fb13652e93a5d726518eae0e2c1e0920b
RicoG53 <104407448+RicoG53@users.noreply.github.com>
2023-04-18 18:42:32 -0700

    Add files via upload

---
+++ library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 12/15/2022 6:38:27 PM
+ * Author : Rico
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
